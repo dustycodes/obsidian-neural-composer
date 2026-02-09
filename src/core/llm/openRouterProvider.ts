e7c12c979457576d7af257a12b6680b38a6416f4
Oscar Campo <oscampo@hotmail.com>
2026-02-09 23:01:30 +0000

    modified:   src/core/llm/openRouterProvider.ts

--- src/core/llm/openRouterProvider.ts
+++ src/core/llm/openRouterProvider.ts
@@ -57,9 +57,6 @@ export class OpenRouterProvider extends BaseLLMProvider<
     return this.adapter.streamResponse(this.client, request, options)
   }
 
-  async getEmbedding(_model: string, _text: string): Promise<number[]> {
-    throw new Error(
-      `Provider ${this.provider.id} does not support embeddings. Please use a different provider.`,
-    )
-  }
+  getEmbedding(_model: string, _text: string): Promise<number[]> {
+  return Promise.reject(new Error(`Provider ${this.provider.id} does not support embeddings. Please use a different provider.`));}
 }
